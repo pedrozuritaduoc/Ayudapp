11c03eb473fcf4f8d745fa0a94aec80f3b6231b8
Pedro Zurita <pe.zurita@duocuc.cl>
2025-07-06 15:55:56 -0400

    cambio v2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,17 @@
-// src/firebase/config.js
+// File: ayudapp/src/firebase/config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAsBbhjyrjOWTfF3ws7s-7-4HFhrC8Dun4",
-  authDomain: "ayudapp-eaa08.firebaseapp.com",
-  projectId: "ayudapp-eaa08",
-  storageBucket: "ayudapp-eaa08.firebasestorage.app",
-  messagingSenderId: "771558705094",
-  appId: "1:771558705094:web:556aec21b6b5516f64d4c7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
